a1b843418ebafd74863b2a13b6c6b7e1d52c5118
Nitin Dhar <nitindhar7@yahoo.com>
2012-01-30 02:33:44 -0500

    basic tabs

---
+++ src/org/nitindhar/android/tab/TestTabActivity.java
@@ -0,0 +1,14 @@
+package org.nitindhar.android.tab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestTabActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+
+}
--- src/org/nitindhar/android/tab/TutorialTabsActivity.java
+++ src/org/nitindhar/android/tab/TutorialTabsActivity.java
@@ -1,13 +1,33 @@
 package org.nitindhar.android.tab;
 
-import android.app.Activity;
+import junit.framework.Test;
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TabHost;
 
-public class TutorialTabsActivity extends Activity {
-    /** Called when the activity is first created. */
+public class TutorialTabsActivity extends TabActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.tabs);
+
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec spec;
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Intent intent = new Intent(this, Test.class);
+        
+        View tab = inflater.inflate(R.layout.tab, getTabWidget(), false);
+        spec = tabHost.newTabSpec("tab").setIndicator(tab).setContent(intent);
+        tabHost.addTab(spec);
+        
+        tab = inflater.inflate(R.layout.tab, getTabWidget(), false);
+        spec = tabHost.newTabSpec("tab").setIndicator(tab).setContent(intent);
+        tabHost.addTab(spec);
     }
 }
